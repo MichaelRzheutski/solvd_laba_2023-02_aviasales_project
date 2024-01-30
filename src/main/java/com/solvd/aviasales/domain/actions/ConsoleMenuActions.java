2f01ab79c87576e64a0d3d57f6f2f8c3f6c70294
Alexey Bobrikov <a.beaverg@gmail.com>
2024-01-31 00:30:13 +0300

    correction of remarks about the console menu actions

---
+++ src/main/java/com/solvd/aviasales/domain/actions/ConsoleMenuActions.java
@@ -0,0 +1,50 @@
+package com.solvd.aviasales.domain.actions;
+
+import com.solvd.aviasales.domain.session.ResultCollector;
+import com.solvd.aviasales.util.JsonParser;
+import com.solvd.aviasales.util.XmlParserJAXB;
+import com.solvd.aviasales.util.ZipArchiver;
+import com.solvd.aviasales.util.console_menu.RequestMethods;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import static com.solvd.aviasales.util.Printers.*;
+
+public class ConsoleMenuActions {
+    protected static final Logger LOGGER = LogManager.getLogger(ConsoleMenuActions.class);
+
+    public static void tearDownActions(ResultCollector result) {
+        File generatedJsonFile;
+        File generatedXmlFile;
+        if (result.getResult().size() > 0) {
+            CollectorActions.showResultCollection(result);
+            generatedJsonFile = JsonParser.saveToJson(result);
+            generatedXmlFile = XmlParserJAXB.saveToXml(result);
+            ZipArchiver.archiveFiles(generatedJsonFile, generatedXmlFile);
+        } else {
+            PRINT2LN.info("[Info]: Result file was not written because there were no actions!");
+        }
+        PRINTLN.info("GOOD BYE!");
+    }
+
+    public static boolean authenticationActions() {
+        String password = RequestMethods.getStringValueFromConsole("admin password");
+        Properties property = new Properties();
+        try (FileInputStream fis = new FileInputStream("src/main/resources/config.properties")) {
+            property.load(fis);
+            if (password.equals(property.getProperty("admin.password"))) {
+                PRINTLN.info("[Info]: Password is correct!");
+                return true;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("You have been problem with reading from property file!", e);
+        }
+        LOGGER.error("[Warning]: Password is incorrect!");
+        return false;
+    }
+}
